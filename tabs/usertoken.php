80996cceedcff0478d7b5f0b2423baebccd99075
Frédéric FRANCE <frederic.france@free.fr>
2025-09-17 22:58:34 +0200

    translation

--- tabs/usertoken.php
+++ tabs/usertoken.php
@@ -1,6 +1,7 @@
 <?php
-/* Copyright (C) 2013-2016  Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2014-2020  Frédéric France      <frederic.france@netlogic.fr>
+/*
+ * Copyright (C) 2013-2016  Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2014-2025  Frédéric France      <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
